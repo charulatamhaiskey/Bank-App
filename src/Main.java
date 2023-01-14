678fe1d43824b71dd2b803e64a939dd4da6bad8d
Charulata Mhaiskey <109023984+charulatamhaiskey@users.noreply.github.com>
2023-01-14 14:24:49 +0530

    Made a change

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,6 @@ public class Main {
 
         // interest
         System.out.println("The interest for 10 years on "+acc1.getBalance()+" Rs will be: "+acc1.calculateInterest(10));
+        System.out.println("Changeddddddddddddddddddddddd ");
     }
 }
